0aaeaca7626c6628823d6fecd8fb64b581fa2904
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-03-23 15:37:31 -0400

    Added image that zooms and rotates.

--- front-page.php
+++ front-page.php
@@ -5,108 +5,112 @@ get_header(); ?>
 	<!-- site-content -->
 	<div class="site-content clearfix">
 
-			<?php if (have_posts()) :
-				while (have_posts()) : the_post();
+		<?php if (have_posts()) :
+			while (have_posts()) : the_post();
 
-				the_content();
+			the_content();
 
-				endwhile;
+			endwhile;
 
-				else :
-					echo '<p>No content found</p>';
+			else :
+				echo '<p>No content found</p>';
 
-				endif; ?>
+			endif; ?>
+			
+			<!-- home-columns -->
+			<div class="home-columns clearfix">
+			
+			<div class="image-container">
+				<div class="the_image"></div>
+			</div>
+
+			<!-- one-half -->
+			<div class="one-half">
 				
-				<!-- home-columns -->
-				<div class="home-columns clearfix">
-					
-					<!-- one-half -->
-					<div class="one-half">
-						
-						<h2>Latest Private</h2>
-						
-						<?php // opinion posts loop begins here
-						$opinionPosts = new WP_Query('cat=4&posts_per_page=2&orderby=title&order=ASC');
+				<h2>Latest Private</h2>
+				
+				<?php // opinion posts loop begins here
+				$opinionPosts = new WP_Query('cat=4&posts_per_page=2&orderby=title&order=ASC');
 
-						if ($opinionPosts->have_posts()) :
+				if ($opinionPosts->have_posts()) :
 
-							while ($opinionPosts->have_posts()) : $opinionPosts->the_post(); ?>
-								<!-- post-item -->
-								<div class="post-item clearfix">
+					while ($opinionPosts->have_posts()) : $opinionPosts->the_post(); ?>
+						<!-- post-item -->
+						<div class="post-item clearfix">
 
-									<!-- post-thumbnail -->
-									<div class="square-thumbnail">
-										<a href="<?php the_permalink(); ?>"><?php
-										if (has_post_thumbnail()) {
-											the_post_thumbnail('square-thumbnail');
-										} else { ?>
+							<!-- post-thumbnail -->
+							<div class="square-thumbnail">
+								<a href="<?php the_permalink(); ?>"><?php
+								if (has_post_thumbnail()) {
+									the_post_thumbnail('square-thumbnail');
+								} else { ?>
 
-											<img src="<?php echo get_template_directory_uri(); ?>/images/leaf.jpg">
+									<img src="<?php echo get_template_directory_uri(); ?>/images/leaf.jpg">
 
-										<?php } ?></a>
-									</div><!-- /post-thumbnail -->
+								<?php } ?></a>
+							</div><!-- /post-thumbnail -->
 
-									<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="subtle-date"><?php the_time('n/j/Y'); ?></span></h4>
+							<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="subtle-date"><?php the_time('n/j/Y'); ?></span></h4>
 
-									<?php the_excerpt(); ?>
+							<?php the_excerpt(); ?>
 
-									</div><!-- /post-item -->
-							<?php endwhile;
+							</div><!-- /post-item -->
+					<?php endwhile;
 
-							else :
-								// fallback no content message here
-						endif;
-						wp_reset_postdata(); ?>
-						
-						<span class="horiz-center"><a href="<?php echo get_category_link(4); ?>" class="btn-a">View all Private Posts</a></span>
-						
-					</div><!-- /one-half -->
-					
-					<!-- one-half -->
-					<div class="one-half last">
-						
-						<h2>Latest Notepad</h2>
-						
-						<?php // news posts loop begins here
-						$newsPosts = new WP_Query('cat=3&posts_per_page=2');
+					else :
+						// fallback no content message here
+				endif;
+				wp_reset_postdata(); ?>
+				
+				<span class="horiz-center"><a href="<?php echo get_category_link(4); ?>" class="btn-a">View all Private Posts</a></span>
+				
+			</div><!-- /one-half -->
+			
+			<!-- one-half -->
+			<div class="one-half last">
+				
+				<h2>Latest Notepad</h2>
+				
+				<?php // news posts loop begins here
+				$newsPosts = new WP_Query('cat=3&posts_per_page=2');
 
-						if ($newsPosts->have_posts()) :
+				if ($newsPosts->have_posts()) :
 
-							while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
-								<!-- post-item -->
-								<div class="post-item clearfix">
+					while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
+						<!-- post-item -->
+						<div class="post-item clearfix">
 
-									<!-- post-thumbnail -->
-									<div class="square-thumbnail">
-										<a href="<?php the_permalink(); ?>"><?php
-										if (has_post_thumbnail()) {
-											the_post_thumbnail('square-thumbnail');
-										} else { ?>
+							<!-- post-thumbnail -->
+							<div class="square-thumbnail">
+								<a href="<?php the_permalink(); ?>"><?php
+								if (has_post_thumbnail()) {
+									the_post_thumbnail('square-thumbnail');
+								} else { ?>
 
-											<img src="<?php echo get_template_directory_uri(); ?>/images/leaf.jpg">
+									<img src="<?php echo get_template_directory_uri(); ?>/images/leaf.jpg">
 
-										<?php } ?></a>
-									</div><!-- /post-thumbnail -->
+								<?php } ?></a>
+							</div><!-- /post-thumbnail -->
 
-									<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="subtle-date"><?php the_time('n/j/Y'); ?></span></h4>
+							<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <span class="subtle-date"><?php the_time('n/j/Y'); ?></span></h4>
 
-									<?php the_excerpt(); ?>
+							<?php the_excerpt(); ?>
 
-									</div><!-- /post-item -->
-							<?php endwhile;
+							</div><!-- /post-item -->
+					<?php endwhile;
 
-							else :
-								// fallback no content message here
-						endif;
-						wp_reset_postdata();
+					else :
+						// fallback no content message here
+				endif;
+				wp_reset_postdata();
 
-						?>
-						
-						<span class="horiz-center"><a href="<?php echo get_category_link(3); ?>" class="btn-a">View all Notepad Posts</a></span>
-						
-					</div><!-- /one-half -->
-					
-				</div><!-- /home-columns -->
+				?>
+				
+				<span class="horiz-center"><a href="<?php echo get_category_link(3); ?>" class="btn-a">View all Notepad Posts</a></span>
+				
+			</div><!-- /one-half -->
+			
+		</div><!-- /home-columns -->
 			
 	</div><!-- /site-content -->
 	
