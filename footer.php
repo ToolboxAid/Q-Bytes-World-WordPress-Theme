88c691be6c684d1275b9b800166a385476364847
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-03-23 12:49:43 -0400

    good base for site (excluding color)

--- footer.php
+++ footer.php
@@ -1,61 +1,65 @@
 
+		</div><!-- /content -->
 
+		<footer class="site-footer">
+			<div class="border"></div>
+					
+				<!-- footer-widgets -->
+				<div class="footer-widgets clearfix">
+					<div class="content-width">							
+						<?php if (is_active_sidebar('footer1')) : ?>				
+							<div class="footer-widget-area">
+								<?php dynamic_sidebar('footer1'); ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php if (is_active_sidebar('footer2')) : ?>
+							<div class="footer-widget-area">
+								<?php dynamic_sidebar('footer2'); ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php if (is_active_sidebar('footer3')) : ?>				
+							<div class="footer-widget-area">
+								<?php dynamic_sidebar('footer3'); ?>
+							</div>
+						<?php endif; ?>
+						
+						<?php if (is_active_sidebar('footer4')) : ?>				
+							<div class="footer-widget-area">
+								<?php dynamic_sidebar('footer4'); ?>
+							</div>
+						<?php endif; ?>
+					</div>					
+				</div><!-- /footer-widgets -->
 
-<footer class="site-footer">
-<div class="border"></div>		
-		<!-- footer-widgets -->
-		<div class="footer-widgets clearfix">
-			
-			<?php if (is_active_sidebar('footer1')) : ?>				
-				<div class="footer-widget-area">
-					<?php dynamic_sidebar('footer1'); ?>
-				</div>
-			<?php endif; ?>
-			
-			<?php if (is_active_sidebar('footer2')) : ?>
-				<div class="footer-widget-area">
-					<?php dynamic_sidebar('footer2'); ?>
-				</div>
-			<?php endif; ?>
-			
-			<?php if (is_active_sidebar('footer3')) : ?>				
-				<div class="footer-widget-area">
-					<?php dynamic_sidebar('footer3'); ?>
-				</div>
-			<?php endif; ?>
-			
-			<?php if (is_active_sidebar('footer4')) : ?>				
-				<div class="footer-widget-area">
-					<?php dynamic_sidebar('footer4'); ?>
-				</div>
-			<?php endif; ?>
-			
-		</div><!-- /footer-widgets -->
-		
-		<nav class="site-nav">
-			<?php			
-			$args = array(
-				'theme_location' => 'footer'
-			);			
-			?>
-			
-			<?php wp_nav_menu(  $args ); ?>
-		</nav>
-		
-		<table class="hrTable">
-			<tr>
-				<td><hr></td>
-				<td class="star">*</td>
-				<td><hr></td>
-			</tr>
-		</table>
-		<div class="cc">
-			<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y');?></p>
-		</div>
-	</footer>
-</div> <!-- class="qbw_body" -->
-</div><!-- container -->
+				<div class="content-width">	
+					<nav class="site-nav content-width">
+						<?php			
+						$args = array(
+							'theme_location' => 'footer'
+						);			
+						?>
+						
+						<?php wp_nav_menu(  $args ); ?>
+					</nav>
+				</div><!-- nav -->
 
-<?php wp_footer(); ?>
-</body>
+				<table class="hrTable">
+					<tr>
+						<td><hr></td>
+						<td class="star">*</td>
+						<td><hr></td>
+					</tr>
+				</table>
+
+				<div class="cc">
+					<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y');?></p>
+				</div>
+			</div>
+		</footer>
+		</div> <!-- class="qbw_body" -->
+		</div><!-- container -->
+		<?php wp_footer(); ?>
+	</body>
 </html>
--- header.php
+++ header.php
@@ -7,16 +7,34 @@
 		<?php wp_head(); ?>
 	</head>
 	
-<body <?php body_class(); ?>>
+	<body <?php body_class(); ?>>
 		<!-- site-header -->
 		<header class="site-header">
 
 			<div class="social">
-				social
+				<div>
+					<div class="content-width">
+						<p>social icons here</p>
+					</div>
+				</div>
 			</div>
 
+			<div class="border"></div>
+			
+			<div class="image">
+				<div class="content-width">
+					<div class="header-search">
+						<?php get_search_form(); ?>
+					</div>
+					
+					<h1 class="title"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+				</div>
+			</div>
+			
+			<div class="border"></div>
+
 			<div class="tagline">
-				<h3><?php bloginfo('description'); ?>
+				<h3 class="content-width"><?php bloginfo('description'); ?>
 					<?php /* if (is_page(7)) {*/
 					if (is_page('quite-kewl')) {?>
 						- Thank you for the visit our KEWL site.
@@ -24,32 +42,14 @@
 				</h3>
 			</div>
 
-			<div class="border"></div>
-			
-			<div class="image">
-			<div>
-					<div class="header-search">
-						<?php get_search_form(); ?>
-					</div>
-
-					<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-
-				</div>
-			</div>
-			
-			<div class="border"></div>
-
 			<div class="menu">				
-				<nav class="site-nav">				
+				<div class="content-width">
+					<nav class="site-nav">				
 					<?php $args = array( 'theme_location' => 'header' ); ?>
 					<?php wp_nav_menu(  $args ); ?>
-				</nav>
+					</nav>
+				</div>
 			</div>
+		</header><!-- /site-header -->
 
-			<div class="container">
-
-
-				
-			</header><!-- /site-header -->
-
-			<div class="qbw_body">
+		<div class="qbw_body content-width"><!-- /content -->
